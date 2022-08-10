9535c5baba6edaa9837d9c702f454860109a1a19
Lyvendia <lyvendia@tutanota.com>
2022-08-10 16:31:49 +0200

    Disable wireplumber autosuspend

--- desktop.nix
+++ desktop.nix
@@ -21,6 +21,48 @@
       };
     };
   };
+  
+  environment.etc = {
+    "wireplumber/main.lua.d/50-alsa-config.lua".text = ''
+      alsa_monitor.enabled = true
+      alsa_monitor.properties = {
+        ["alsa.reserve"] = true,
+        ["alsa.midi"] = true,
+        ["alsa.midi.monitoring"] = true,
+        ["vm.node.defaults"] = {
+          ["api.alsa.period-size"] = 256,
+          ["api.alsa.headroom"] = 8192,
+        },
+      }
+      alsa_monitor.rules = {
+        {
+          matches = {
+            {
+              { "device.name", "matches", "alsa_card.*" },
+            },
+          },
+          apply_properties = {
+            ["api.alsa.use-acp"] = true,
+            ["api.acp.auto-profile"] = false,
+            ["api.acp.auto-port"] = false,
+          },
+        },
+        {
+          matches = {
+            {
+              { "node.name", "matches", "alsa_input.*" },
+            },
+            {
+              { "node.name", "matches", "alsa_output.*" },
+            },
+          },
+          apply_properties = {
+            ["session.suspend-timeout-seconds"] = 0
+          },
+        },
+      }
+    '';
+  };
 
   networking.firewall = {
     extraCommands = ''
